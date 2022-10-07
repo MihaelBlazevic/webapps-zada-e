9c0b3083ce85a5facdc707a3006a9c143b0a729b
unknown <mihael75.blazevic@gmail.com>
2022-10-07 18:32:02 +0200

    Rjesene Zadatak 4

---
+++ 01-Zadace/zadatak4.js
@@ -0,0 +1,14 @@
+let provjera = (arr) => {
+    for (let i of arr) {
+        if (arr[i] % 3 === 0) {
+            console.log(arr[i])
+        }
+        else {
+
+        }
+    }
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+provjera(arr)
